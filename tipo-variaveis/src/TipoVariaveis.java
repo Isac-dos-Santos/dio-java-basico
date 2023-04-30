b5b7bd4a72e1be9a13af8baf98382222ab6f137e
isacd <isacdossantossilva37@gmail.com>
2023-04-30 18:28:38 -0300

    Exemplos de tipos primitivos

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,12 @@
 public class TipoVariaveis {
+    // TIPOIS PRIMITIVOS
+    // int, byte, short, float, double, boolean e char
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salario = 2500;
+        float altura = 1.70F;//float tem que finalizar com um F
+        long cpf = 64235679202L; 
+
+        //constante = valor imutavel -> ESCRITA EM CAIXA ALTA
+        final  double VALOR_DE_PI = 3.14; // "final" determina que uma constante
     }
 }
